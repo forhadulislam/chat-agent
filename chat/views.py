c1aa73cf004d416b8b3b049a0eb054e34f2c6683
Sadi <ronex.sadi@yahoo.com>
2017-06-27 19:24:00 +0300

    updated

--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from nltk.corpus import nps_chat
 # Create your views here.
 
 def index(request):
-    
+    posts = nps_chat.posts()
     return render(request,'index.html', {
-        'categories': '',
+        'posts': posts,
     })
